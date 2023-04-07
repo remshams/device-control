18f4e38c2710a92579ad44d043b756c4967c8d25
Mathias Remshardt <remshams@gmail.com>
2023-04-07 14:29:55 +0200

    feat(Keylight): adds shared test module

--- src/keylight.rs
+++ src/keylight.rs
@@ -7,3 +7,57 @@ pub use keylight::{Keylight, KeylightMetadata, Light};
 pub use keylight_adapter::KeylightRestAdapter;
 pub use keylight_control::{KeylightAdapter, KeylightControl, KeylightFinder};
 pub use keylight_finder::ZeroConfKeylightFinder;
+
+#[cfg(test)]
+mod keylight_mocks {
+
+    use super::{keylight::KeylightError, *};
+
+    pub struct MockKeylightFinder {
+        pub metadata: Vec<KeylightMetadata>,
+    }
+
+    impl KeylightFinder for MockKeylightFinder {
+        type Output = Vec<KeylightMetadata>;
+
+        fn discover(&self) -> Self::Output {
+            self.metadata.clone()
+        }
+    }
+
+    impl MockKeylightFinder {
+        pub fn new(metadata: Vec<KeylightMetadata>) -> MockKeylightFinder {
+            MockKeylightFinder { metadata }
+        }
+    }
+
+    pub struct MockKeylightAdapter {
+        pub lights: Vec<Light>,
+    }
+
+    impl KeylightAdapter for MockKeylightAdapter {
+        fn lights(&self, ip: &String) -> Result<Vec<Light>, KeylightError> {
+            Ok(self.lights.clone())
+        }
+    }
+
+    pub fn create_metadata_fixture() -> Vec<KeylightMetadata> {
+        vec![
+            KeylightMetadata {
+                name: String::from("first"),
+                ip: String::from("102.168.1.1"),
+                port: 1234,
+            },
+            KeylightMetadata {
+                name: String::from("second"),
+                ip: String::from("102.168.1.2"),
+                port: 4567,
+            },
+            KeylightMetadata {
+                name: String::from("first"),
+                ip: String::from("102.168.1.1"),
+                port: 1234,
+            },
+        ]
+    }
+}
--- src/keylight/keylight_control.rs
+++ src/keylight/keylight_control.rs
@@ -45,57 +45,15 @@ impl<'a, F: KeylightFinder, A: KeylightAdapter> KeylightControl<'a, F, A> {
 mod test {
 
     use crate::keylight::keylight::KeylightMetadata;
+    use crate::keylight::keylight_mocks::{
+        create_metadata_fixture, MockKeylightAdapter, MockKeylightFinder,
+    };
 
     use super::*;
 
-    struct MockKeylightFinder {
-        pub metadata: Vec<KeylightMetadata>,
-    }
-
-    impl KeylightFinder for MockKeylightFinder {
-        type Output = Vec<KeylightMetadata>;
-
-        fn discover(&self) -> Self::Output {
-            self.metadata.clone()
-        }
-    }
-
-    impl MockKeylightFinder {
-        fn new(metadata: Vec<KeylightMetadata>) -> MockKeylightFinder {
-            MockKeylightFinder { metadata }
-        }
-    }
-
-    struct MockKeylightAdapter {
-        pub lights: Vec<Light>,
-    }
-
-    impl KeylightAdapter for MockKeylightAdapter {
-        fn lights(&self, ip: &String) -> Result<Vec<Light>, KeylightError> {
-            Ok(self.lights.clone())
-        }
-    }
-
     fn prepare_test() -> (MockKeylightFinder, MockKeylightAdapter) {
-        let test_metadata: Vec<KeylightMetadata> = vec![
-            KeylightMetadata {
-                name: String::from("first"),
-                ip: String::from("102.168.1.1"),
-                port: 1234,
-            },
-            KeylightMetadata {
-                name: String::from("second"),
-                ip: String::from("102.168.1.2"),
-                port: 4567,
-            },
-            KeylightMetadata {
-                name: String::from("first"),
-                ip: String::from("102.168.1.1"),
-                port: 1234,
-            },
-        ];
         (
-            MockKeylightFinder::new(test_metadata),
+            MockKeylightFinder::new(create_metadata_fixture()),
             MockKeylightAdapter { lights: vec![] },
         )
     }

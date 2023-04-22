407174361e1329098a91ed7d6562fd5199ed7195
Mathias Remshardt <remshams@gmail.com>
2023-04-22 18:09:40 +0200

    feat(main): updates implementation for loading keylights
    Now uses the new "load_lights" function.

--- src/display/progress.rs
+++ src/display/progress.rs
@@ -2,7 +2,11 @@ use std::time::Duration;
 
 use indicatif::{ProgressBar, ProgressStyle};
 
-pub fn run<F: FnMut() -> ()>(mut action: F, pending_message: String, finished_message: String) {
+pub fn run<R, F: FnMut() -> R>(
+    mut action: F,
+    pending_message: String,
+    finished_message: String,
+) -> R {
     let spinner = ProgressBar::new_spinner();
     spinner.enable_steady_tick(Duration::from_millis(100));
     spinner.set_style(
@@ -12,6 +16,7 @@ pub fn run<F: FnMut() -> ()>(mut action: F, pending_message: String, finished_me
             .unwrap(),
     );
     spinner.set_message(pending_message);
-    action();
+    let result = action();
     spinner.finish_with_message(finished_message);
+    result
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,21 @@
 use keylight_on::display;
 use keylight_on::keylight::{
-    KeylightControl, KeylightError, KeylightRestAdapter, ZeroConfKeylightFinder,
+    KeylightControl, KeylightError, KeylightJsonDb, KeylightRestAdapter, ZeroConfKeylightFinder,
 };
 mod cli;
 
 fn main() -> Result<(), KeylightError> {
     let command_light = cli::parse();
-    println!("{:?}", command_light);
     let finder = ZeroConfKeylightFinder::new();
     let adapter = KeylightRestAdapter {};
-    let mut keylight_control = KeylightControl::new(&finder, &adapter);
-    let action = || {
-        keylight_control.discover_lights();
-    };
+    let db = KeylightJsonDb::new(String::from("./testing.json"));
+    let mut keylight_control = KeylightControl::new(&finder, &adapter, &db);
+    let action = || keylight_control.load_lights();
     display::progress::run(
         action,
         String::from("Discovering lights"),
         String::from("Lights discovered"),
-    );
+    )?;
     let mut keylights = keylight_control.lights;
     let light = keylights.get_mut(0).unwrap();
     light.lights()?;

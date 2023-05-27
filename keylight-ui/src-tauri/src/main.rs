d4e7f9a691c322e2e1b00d4d556980140338e94c
Mathias Remshardt <remshams@gmail.com>
2023-05-27 11:38:00 +0200

    feat(ui-main): adds setup of app state

--- keylight-ui/src-tauri/src/main.rs
+++ keylight-ui/src-tauri/src/main.rs
@@ -1,6 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::sync::Mutex;
+
+use keylight_control::keylight::{
+    KeylightControl, KeylightJsonDb, KeylightRestAdapter, ZeroConfKeylightFinder, KEYLIGHT_DB_PATH,
+};
+use model::AppState;
+
+use crate::keylight::discover_keylights;
+
+mod keylight;
+mod model;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,8 +20,17 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let finder = ZeroConfKeylightFinder::new();
+    let db = KeylightJsonDb::new(KEYLIGHT_DB_PATH);
+    let adapter = KeylightRestAdapter {};
+    let control = KeylightControl::new(finder, db);
+    let app_state = AppState {
+        adapter,
+        keylight_control: Mutex::new(control),
+    };
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(app_state)
+        .invoke_handler(tauri::generate_handler![greet, discover_keylights])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }

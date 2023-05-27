c98e5baedcd4550d1a42bf359e0195124eacf826
Mathias Remshardt <remshams@gmail.com>
2023-05-27 13:40:30 +0200

    feat(ui-keylight): adds loading of keylights in tauri

---
+++ keylight-ui/src/keylight/keylight.ts
@@ -0,0 +1,30 @@
+import { invoke } from '@tauri-apps/api';
+
+export type KeylightMetadata = {
+  name: string;
+  ip: number;
+  port: number;
+};
+
+export type Keylight = {
+  metadata: KeylightMetadata;
+  lights: Array<Light>;
+};
+
+export type Light = {
+  on: boolean;
+  temperature: number;
+  brightness: number;
+};
+
+export const KeylightError = {
+  keylightLoadError: 'keylightLoadError'
+} as const;
+export type KeylightError = keyof typeof KeylightError;
+
+export const loadKeylights = async (): Promise<Array<Keylight>> => {
+  const result = await invoke<Array<Keylight>>('discover_keylights').catch(_e => {
+    throw KeylightError.keylightLoadError;
+  });
+  return result;
+};

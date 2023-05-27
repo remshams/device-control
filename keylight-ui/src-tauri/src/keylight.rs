70dba7070387af3c051b775c59f1d0026a26e78d
Mathias Remshardt <remshams@gmail.com>
2023-05-27 11:37:38 +0200

    feat(ui-keylight): adds discover command

---
+++ keylight-ui/src-tauri/src/keylight.rs
@@ -0,0 +1,18 @@
+use keylight_control::keylight::KeylightError;
+use tauri::State;
+
+use crate::model::AppState;
+
+#[tauri::command]
+pub fn discover_keylights(state: State<AppState>) -> Result<(), KeylightError> {
+    let mut keylight_control = state
+        .keylight_control
+        .lock()
+        .map_err(|_err| KeylightError::CommandError(String::from("test")))?;
+    keylight_control.load_keylights()?;
+    let lights = &mut keylight_control.lights;
+    for light in lights.iter_mut() {
+        light.lights(&state.adapter)?;
+    }
+    Ok(())
+}

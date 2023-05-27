fea4ce4001fcfd7d7286c95a4ec659de7afebf68
Mathias Remshardt <remshams@gmail.com>
2023-05-27 11:37:25 +0200

    feat(ui-app): adds appstate model

---
+++ keylight-ui/src-tauri/src/model.rs
@@ -0,0 +1,10 @@
+use std::sync::Mutex;
+
+use keylight_control::keylight::{
+    KeylightControl, KeylightJsonDb, KeylightRestAdapter, ZeroConfKeylightFinder,
+};
+
+pub struct AppState {
+    pub adapter: KeylightRestAdapter,
+    pub keylight_control: Mutex<KeylightControl<ZeroConfKeylightFinder, KeylightJsonDb<'static>>>,
+}

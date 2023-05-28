6eddab1af64df7be46ce8bcc4e3f0565d2e6db64
Mathias Remshardt <remshams@gmail.com>
2023-05-28 11:27:18 +0200

    feat(sc-Keylight): adds rendering keylight metadata values

--- keylight-ui/src/keylight/keylight.tsx
+++ keylight-ui/src/keylight/keylight.tsx
@@ -1,3 +1,22 @@
-export const Keylight = () => {
-  return <span>Content</span>;
+import { Keylight as KeylightModel } from './adapter';
+import styles from './Keylight.module.css';
+
+export const Keylight = ({ light }: { light: KeylightModel }) => {
+  return (
+    <div class={styles.keylight}>
+      <div class={styles.lightSwitch}>
+        <button>{light.lights[0].on ? 'On' : 'Off'}</button>
+      </div>
+      <div class={styles.metadata}>
+        <div>
+          <label>Temperature:</label>
+          <span>{light.lights[0].temperature}</span>
+        </div>
+        <div>
+          <label>Brightness:</label>
+          <span>{light.lights[0].brightness}</span>
+        </div>
+      </div>
+    </div>
+  );
 };

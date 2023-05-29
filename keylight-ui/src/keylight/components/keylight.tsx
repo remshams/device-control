6f36ab13849a434f331201730d6ebd38d3c421bd
Mathias Remshardt <remshams@gmail.com>
2023-05-29 10:20:33 +0200

    feat(ui-keylight): adds setting light
    The current version has a bug that a new value can only be set
    after the updated value from the system has been returned.
    
    This will be fixed in later commits

--- keylight-ui/src/keylight/components/keylight.tsx
+++ keylight-ui/src/keylight/components/keylight.tsx
@@ -1,4 +1,4 @@
-import { Keylight as KeylightModel } from '../adapter';
+import { Keylight as KeylightModel, setLight as setLightSystem } from '../adapter';
 import styles from './Keylight.module.css';
 
 type KeyValueProps = {
@@ -18,10 +18,13 @@ const KeyValue = ({ label, value }: KeyValueProps) => (
 );
 
 export const Keylight = (props: KeylightProps) => {
+  const toggleOn = () => {
+    setLightSystem({ id: props.light.metadata.id, index: 0, on: !props.light.lights[0].on });
+  };
   return (
     <div class={styles.keylight}>
       <div class={styles.lightSwitch}>
-        <button>{props.light.lights[0].on ? 'On' : 'Off'}</button>
+        <button onClick={toggleOn}>{props.light.lights[0].on ? 'On' : 'Off'}</button>
       </div>
       <div class={styles.metadata}>
         <KeyValue label="Temperature" value={props.light.lights[0].temperature.toString()} />

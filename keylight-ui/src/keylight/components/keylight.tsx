bfaca94ced1c51c6a86883917a42cc104853ad95
Mathias Remshardt <remshams@gmail.com>
2023-06-03 10:30:41 +0200

    feat(ui-keylight): adds slider for setting temperature and brightness

--- keylight-ui/src/keylight/components/keylight.tsx
+++ keylight-ui/src/keylight/components/keylight.tsx
@@ -1,6 +1,9 @@
 import { createEffect, createSignal } from 'solid-js';
 import { Keylight as KeylightModel, setKeylight } from '../adapter';
+import { areLightsEditedSignal } from '../stores';
 import styles from './Keylight.module.css';
+
+const [_areLightsEdited, setAreLightsEdited] = areLightsEditedSignal;
 
 type KeyValueProps = {
   label: string;
@@ -11,18 +14,39 @@ type KeylightProps = {
   light: KeylightModel;
 };
 
-const KeyValue = ({ label, value }: KeyValueProps) => (
+const KeyValue = (props: KeyValueProps) => (
   <div class={styles.keyValue}>
-    <label>{label}:</label>
-    <span>{value}</span>
+    <label>{props.label}:</label>
+    <span>{props.value}</span>
+  </div>
+);
+
+type LightSliderProps = {
+  label: string;
+  value: number;
+  min?: number;
+  max?: number;
+  onChange: (value: number) => void;
+};
+
+const LightSlider = (props: LightSliderProps) => (
+  <div class={styles.lightSlider}>
+    <label for="slider">{props.label}</label>
+    <input
+      id="slider"
+      type="range"
+      min={props.min ?? 0}
+      max={props.max ?? 100}
+      value={props.value}
+      onChange={event => props.onChange(Number(event.currentTarget.value))}
+      onMouseDown={_event => setAreLightsEdited(true)}
+      onMouseUp={_event => setAreLightsEdited(false)}
+    />
   </div>
 );
 
 export const Keylight = (props: KeylightProps) => {
   const [light, setLight] = createSignal(props.light.light);
-  const toggleOn = () => {
-    setLight({ ...light(), on: !light().on });
-  };
   createEffect(() => {
     setKeylight({ id: props.light.metadata.id, index: 0, ...light() });
   });
@@ -30,11 +54,27 @@ export const Keylight = (props: KeylightProps) => {
   return (
     <div class={styles.keylight}>
       <div class={styles.lightSwitch}>
-        <button onClick={toggleOn}>{light().on ? 'On' : 'Off'}</button>
+        <button onClick={() => setLight({ ...light(), on: !light().on })}>{light().on ? 'On' : 'Off'}</button>
       </div>
       <div class={styles.metadata}>
-        <KeyValue label="Temperature" value={light().temperature.toString()} />
-        <KeyValue label="Brightness" value={light().brightness.toString()} />
+        <div>
+          <LightSlider
+            label="Temperature"
+            value={light().temperature}
+            min={143}
+            max={344}
+            onChange={value => setLight({ ...light(), temperature: value })}
+          />
+          <KeyValue label="Temperature" value={light().temperature.toString()} />
+        </div>
+        <div>
+          <LightSlider
+            label="Brightness"
+            value={light().brightness}
+            onChange={value => setLight({ ...light(), brightness: value })}
+          />
+          <KeyValue label="Brightness" value={light().brightness.toString()} />
+        </div>
       </div>
     </div>
   );

2a2e5ae443dbd3602dfce0bf22c534b862e67388
Mathias Remshardt <remshams@gmail.com>
2023-11-01 15:58:19 +0100

    feat(go-HueControl-Bridge): adds getters for properties

--- go/hue-control/internal/bridges/bridge.go
+++ go/hue-control/internal/bridges/bridge.go
@@ -12,6 +12,14 @@ type Bridge struct {
 	apiKey string
 }
 
+func (bridge Bridge) GetIp() net.IP {
+	return bridge.ip
+}
+
+func (bridge Bridge) GetApiKey() string {
+	return bridge.apiKey
+}
+
 func InitBridge(ip net.IP, apiKey string) Bridge {
 	return Bridge{ip: ip, apiKey: apiKey}
 }

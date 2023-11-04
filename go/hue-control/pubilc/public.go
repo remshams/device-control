6e6a4e71ed4c7685cfc794b912ad51170552b7dc
Mathias Remshardt <remshams@gmail.com>
2023-11-04 10:44:25 +0100

    feat(go-Hue): adds helper for hue control init

--- go/hue-control/pubilc/public.go
+++ go/hue-control/pubilc/public.go
@@ -1,9 +1,22 @@
 package hue_control
 
 import (
-	control "hue-control/internal"
+	"hue-control/internal"
+	"hue-control/internal/bridges"
 	"hue-control/internal/groups"
 )
 
 type Group = groups.Group
 type Control = control.HueControl
+
+func InitHueControl(bridgesStore *bridges.BridgesStore) control.HueControl {
+	if bridgesStore != nil {
+		return control.InitHueControl(*bridgesStore)
+	} else {
+		return control.InitHueControl(bridges.InitBridgesJsonStore())
+	}
+}
+
+func InitBridgesJsonStore() bridges.BridgesJsonStore {
+	return bridges.InitBridgesJsonStore()
+}

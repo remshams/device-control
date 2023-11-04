7a0634b2c22dccba5cbe9e6092e8958078fe22c1
Mathias Remshardt <remshams@gmail.com>
2023-11-04 15:01:56 +0100

    feat(go-HueControl-Groups): adds getters

--- go/hue-control/internal/groups/group.go
+++ go/hue-control/internal/groups/group.go
@@ -1,11 +1,19 @@
 package groups
 
+type GroupAdapter interface {
+	All() ([]Group, error)
+}
+
 type Group struct {
 	id     string
 	name   string
 	lights []string
 }
 
-type GroupAdapter interface {
-	All() ([]Group, error)
+func (group Group) GetId() string {
+	return group.id
+}
+
+func (group Group) GetName() string {
+	return group.name
 }

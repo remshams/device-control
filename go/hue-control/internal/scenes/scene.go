87e704a2beac45c8850f78cc8e3ebb7b6bc196d7
Mathias Remshardt <remshams@gmail.com>
2023-11-25 11:30:50 +0100

    feat(go-Keylight-HueControl): adds scene model

---
+++ go/hue-control/internal/scenes/scene.go
@@ -0,0 +1,33 @@
+package scenes
+
+type Scene struct {
+	id        string
+	name      string
+	groupId   string
+	sceneType string
+}
+
+type SceneAdapter interface {
+	All(groupId string) ([]Scene, error)
+}
+
+func InitScene(id string, name string, groupId string, sceneType string) Scene {
+	return Scene{
+		id:        id,
+		name:      name,
+		groupId:   groupId,
+		sceneType: sceneType,
+	}
+}
+
+func (s Scene) Name() string {
+	return s.name
+}
+
+func (s Scene) GroupId() string {
+	return s.groupId
+}
+
+func (s Scene) SceneType() string {
+	return s.sceneType
+}

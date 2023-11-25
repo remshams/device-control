112b3f70b950a48281a50450d11b8fef2b8a01de
Mathias Remshardt <remshams@gmail.com>
2023-11-25 11:49:55 +0100

    feat(go-Keyolight-HueControl): adds request with timeout helper method

--- go/hue-control/internal/http/utils.go
+++ go/hue-control/internal/http/utils.go
@@ -1,8 +1,11 @@
 package hue_control_http
 
 import (
+	"context"
+	"io"
 	"net/http"
 	"strings"
+	"time"
 )
 
 func HasError(res *http.Response, body *[]byte) bool {
@@ -12,4 +15,16 @@ func HasError(res *http.Response, body *[]byte) bool {
 	} else {
 		return false
 	}
+}
+
+func RequestWithTimeout(method string, url string, body io.Reader, timeout *time.Duration) (*http.Request, *http.Client, context.CancelFunc, error) {
+	defaultTimeout := 2 * time.Second
+	requestTimeout := timeout
+	if requestTimeout == nil {
+		requestTimeout = &defaultTimeout
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), *requestTimeout)
+	client := &http.Client{}
+	req, err := http.NewRequestWithContext(ctx, method, url, body)
+	return req, client, cancel, err
 }

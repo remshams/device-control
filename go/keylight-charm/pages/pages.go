1601bbfafb0b347923a1bc9446154b16e16117ac
Mathias Remshardt <remshams@gmail.com>
2023-12-02 15:18:26 +0100

    feat(go-KeylightCharm): adds WindowResizeAction
    Is triggered when resize event of the application window happens

--- go/keylight-charm/pages/pages.go
+++ go/keylight-charm/pages/pages.go
@@ -9,3 +9,25 @@ func CreateBackToMenuAction() tea.Cmd {
 		return BackToMenuAction{}
 	}
 }
+
+type WindowResizeAction struct {
+	Width  int
+	Height int
+}
+
+func CreateWindowResizeAction(width int, height int) tea.Cmd {
+	return func() tea.Msg {
+		return WindowResizeAction{
+			Width:  width,
+			Height: height,
+		}
+	}
+}
+
+func IsSystemMsg(msg tea.Msg) bool {
+	switch msg.(type) {
+	case tea.WindowSizeMsg:
+		return true
+	}
+	return false
+}

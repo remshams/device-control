352199130de4975ca611fe9f4e177c31b711e142
Mathias Remshardt <remshams@gmail.com>
2023-12-02 11:11:28 +0100

    feat(go-KeylightCharm-Hue): adds scene to public interface

--- go/hue-control/pubilc/public.go
+++ go/hue-control/pubilc/public.go
@@ -4,10 +4,12 @@ import (
 	"hue-control/internal"
 	"hue-control/internal/bridges"
 	"hue-control/internal/groups"
+	"hue-control/internal/scenes"
 )
 
 type Bridge = bridges.Bridge
 type Group = groups.Group
+type Scene = scenes.Scene
 type Control = control.HueControl
 
 func InitHueControl(bridgesStore bridges.BridgesStore) control.HueControl {

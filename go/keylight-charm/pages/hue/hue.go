48d312daec29b8bb002f596743f72679c9866afe
Mathias Remshardt <remshams@gmail.com>
2023-12-19 10:52:25 +0100

    feat(go-KeylightCharm-Hue): adds bridges reloaded action

--- go/keylight-charm/pages/hue/hue.go
+++ go/keylight-charm/pages/hue/hue.go
@@ -1,12 +1,28 @@
 package pages_hue
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	hue_control "hue-control/pubilc"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
 
 type ReloadBridgesAction struct{}
 
 func CreateReloadBridgesAction() tea.Cmd {
 	return func() tea.Msg {
 		return ReloadBridgesAction{}
+	}
+}
+
+type BridgesReloadedAction struct {
+	Bridges []hue_control.Bridge
+}
+
+func CreateBridgesReloadedAction(bridges []hue_control.Bridge) tea.Cmd {
+	return func() tea.Msg {
+		return BridgesReloadedAction{
+			Bridges: bridges,
+		}
 	}
 }
 

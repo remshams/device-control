62d0b3b8f9f88651b903bd492d2f9238b4a9adee
Mathias Remshardt <remshams@gmail.com>
2023-12-22 15:57:04 +0100

    feat(go-HueControl-Lights): adds all method in LightsHttpAdapter
    Returns all registered lights for the bridge

---
+++ go/hue-control/internal/lights/adapter.go
@@ -0,0 +1,91 @@
+package lights
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	hue_control_http "hue-control/internal/http"
+	"io"
+	"net"
+	"net/http"
+
+	"github.com/charmbracelet/log"
+)
+
+const path = "http://%s/api/%s/lights"
+
+type lightDtoById map[string]lightDto
+
+type lightDto struct {
+	Name  string        `json:"name"`
+	State lightStateDto `json:"state"`
+}
+
+type lightStateDto struct {
+	On  bool `json:"on"`
+	Bri int  `json:"bri"`
+	Hue int  `json:"hue"`
+	Sat int  `json:"sat"`
+}
+
+func (lightDto lightDto) toLight(id string) Light {
+	return InitLight(
+		id,
+		lightDto.Name,
+		lightDto.State.On,
+		lightDto.State.Bri,
+		lightDto.State.Hue,
+		lightDto.State.Sat,
+	)
+}
+
+func parseResponse(body []byte) ([]Light, error) {
+	var lightResponseDto lightDtoById
+	err := json.Unmarshal(body, &lightResponseDto)
+	if err != nil {
+		log.Errorf("Failed to parse lights: %v", err)
+		return nil, errors.New("Failed to parse lights")
+	}
+	if len(lightResponseDto) == 0 {
+		return []Light{}, nil
+	}
+	lights := []Light{}
+	for id, lightDto := range lightResponseDto {
+		lights = append(lights, lightDto.toLight(id))
+	}
+	return lights, nil
+}
+
+type LightHttpAdapter struct {
+	ip     net.IP
+	apiKey string
+}
+
+func InitLightHttpAdapter(ip net.IP, apiKey string) LightHttpAdapter {
+	return LightHttpAdapter{
+		ip:     ip,
+		apiKey: apiKey,
+	}
+}
+
+func (adapter LightHttpAdapter) All() ([]Light, error) {
+	req, client, cancel, err := hue_control_http.RequestWithTimeout(
+		http.MethodGet,
+		fmt.Sprintf(path, adapter.ip.String(), adapter.apiKey),
+		nil,
+		nil,
+	)
+	defer cancel()
+	res, err := client.Do(req)
+	if err != nil {
+		log.Errorf("Failed to create lights request: %v", err)
+		return nil, errors.New("Failed to create lights request")
+	}
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil || hue_control_http.HasError(res, &body) {
+		return nil, errors.New("Failed to get lights")
+	}
+	return parseResponse(body)
+
+}

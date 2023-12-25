12421556922f09babc61acb609cd810dfce05026
Mathias Remshardt <remshams@gmail.com>
2023-12-25 11:39:38 +0100

    feat(go-KeylightCharm-LightsHome): adds forwarding update to subviews

--- go/keylight-charm/pages/hue/lights/home/home.go
+++ go/keylight-charm/pages/hue/lights/home/home.go
@@ -3,6 +3,7 @@ package hue_lights_home
 import (
 	hue_control "hue-control/pubilc"
 	"keylight-charm/lights/hue"
+	pages_hue "keylight-charm/pages/hue"
 	hue_lights "keylight-charm/pages/hue/lights"
 	hue_lights_details "keylight-charm/pages/hue/lights/details"
 	hue_lights_list "keylight-charm/pages/hue/lights/list"
@@ -43,19 +44,28 @@ func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 	case hue_lights.BackToLightHomeAction:
 		m.details = nil
 		m.state = list
+		cmd = pages_hue.CreateBackToHueHomeAction()
 	case hue_lights_list.LightSelected:
 		detailsModel := hue_lights_details.InitModel(m.adapter, msg.Light)
 		m.details = &detailsModel
 		m.state = details
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "enter":
-		}
-
 	default:
-		m.list, cmd = m.list.Update(msg)
+		cmd = m.forwardAction(msg)
 	}
 	return m, cmd
+}
+
+func (m *Model) forwardAction(msg tea.Msg) tea.Cmd {
+	var cmd tea.Cmd
+	switch m.state {
+	case list:
+		m.list, cmd = m.list.Update(msg)
+	case details:
+		details, detailsCmd := m.details.Update(msg)
+		m.details = &details
+		cmd = detailsCmd
+	}
+	return cmd
 }
 
 func (m Model) View() string {

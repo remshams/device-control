e9d3976ef87033eda345d45450412fc18388f7d9
Mathias Remshardt <remshams@gmail.com>
2024-01-14 17:12:41 +0100

    feat(settings-public): adds public settings interface
    Adds methods for initializing settings and its dependencies

--- settings/public/public.go
+++ settings/public/public.go
@@ -3,5 +3,30 @@ package device_control_settings
 import "github.com/remshams/device-control/settings/internal/settings"
 
 type Settings = settings.Settings
+type Location = settings.Location
+type SunriseAndSunset = settings.SunriseAndSunset
 
 var StorePath = ".config/device-control/settings.json"
+
+func InitSettingsJsonStore(path string) settings.SettingsStore {
+	return settings.InitSettingsJsonStore(path)
+}
+
+func InitSunriseAndSunsetOrgAdapter() settings.SunriseAndSunsetAdapter {
+	return settings.InitSunriseAndSunsetOrgAdapter()
+}
+
+func LoadSettings(
+	store settings.SettingsStore,
+	sunriseSunsetAdapter settings.SunriseAndSunsetAdapter,
+) (*Settings, error) {
+	return settings.InitFromStore(store, sunriseSunsetAdapter)
+}
+
+func InitSettings(
+	store settings.SettingsStore,
+	sunriseSunsetAdapter settings.SunriseAndSunsetAdapter,
+	location settings.Location,
+) (*Settings, error) {
+	return settings.InitSettings(store, sunriseSunsetAdapter, location)
+}

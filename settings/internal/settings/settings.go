cdb9de0225fef5aea21d4cb1d2d999701f973e2a
Mathias Remshardt <remshams@gmail.com>
2024-01-14 12:11:28 +0100

    feat(settings): adds sunrise and -set to model

--- settings/internal/settings/settings.go
+++ settings/internal/settings/settings.go
@@ -1,30 +1,47 @@
 package settings
 
+import "time"
+
 type SettingsStore interface {
 	Save(settings Settings) error
 	Load() (*Settings, error)
 }
 
-type Settings struct {
-	store      SettingsStore
+type Location struct {
 	longtitude float64
 	latitude   float64
 }
 
+type SunriseAndSunset struct {
+	sunrise time.Time
+	sunset  time.Time
+}
+
+type Settings struct {
+	store            SettingsStore
+	location         Location
+	sunriseAndSunset SunriseAndSunset
+}
+
 func InitSettings(store SettingsStore, longtitude float64, latitude float64) Settings {
 	return Settings{
-		longtitude: longtitude,
-		latitude:   latitude,
-		store:      store,
+		location: Location{
+			longtitude: longtitude,
+			latitude:   latitude,
+		},
+		store: store,
 	}
 }
 
 func (settings Settings) GetLongtitude() float64 {
-	return settings.longtitude
+	return settings.location.longtitude
 }
 
 func (settings Settings) GetLatitude() float64 {
-	return settings.latitude
+	return settings.location.latitude
+}
+
+func (settings *Settings) UpdateSunriseAndSunset() {
 }
 
 func (settings Settings) Save() error {

6ecbc6c1de7f0821fbdee762977d52bdb336e372
Mathias Remshardt <remshams@gmail.com>
2024-01-20 11:22:49 +0100

    feat(tui-Textinput): implements edit mode

--- tui/components/dc_textinput/textinput.go
+++ tui/components/dc_textinput/textinput.go
@@ -31,10 +31,18 @@ var TextInputKeyMap = KeyMap{
 	),
 }
 
+type viewState string
+
+const (
+	navigate viewState = "navigate"
+	edit     viewState = "edit"
+)
+
 type Model struct {
 	label string
 	unit  string
 	Input textinput.Model
+	state viewState
 }
 
 func New(label string, unit string) Model {
@@ -42,6 +50,7 @@ func New(label string, unit string) Model {
 		label: label,
 		unit:  unit,
 		Input: CreateTextInputModel(),
+		state: navigate,
 	}
 }
 
@@ -55,11 +64,20 @@ func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 	case tea.KeyMsg:
 		switch {
 		case key.Matches(msg, TextInputKeyMap.Edit):
-			m.Input.Focus()
+			if m.state == navigate {
+				m.state = edit
+				m.Input.Focus()
+			} else {
+				m.Input, cmd = m.Input.Update(msg)
+			}
 		case key.Matches(msg, TextInputKeyMap.Discard):
+			m.state = navigate
 			m.Input.Blur()
 		case key.Matches(msg, TextInputKeyMap.Apply):
+			m.state = navigate
 			m.Input.Blur()
+		default:
+			m.Input, cmd = m.Input.Update(msg)
 		}
 	}
 	return m, cmd
